3acb55792b93d613c355a2a1461323a848213ceb
Jaimin Parmar <jaimins100@gmail.com>
2022-12-31 11:34:31 +0530

    campgrounds show route

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/yelpcamp');
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'))
-db.on('open', () => { 
-    console.log('Database connected');
-})
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', () => {
+  console.log('Database connected');
+});
 
 const app = express();
 
@@ -19,14 +19,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000');
+  console.log('Serving on port 3000');
 });
