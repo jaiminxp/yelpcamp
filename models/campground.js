e529b5bc9aa61db751392c8943eaca972ed1ec5f
Jaimin Parmar <jaimins100@gmail.com>
2023-01-21 12:14:08 +0530

    adding images from unsplash

--- models/campground.js
+++ models/campground.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: String,
-    price: String,
-    description: String,
-    location: String
-})
+  title: String,
+  price: Number,
+  image: String,
+  description: String,
+  location: String,
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors  } = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.set('strictQuery', true);
@@ -12,24 +12,30 @@ db.on('open', () => {
   console.log('Database connected');
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
+  await Campground.deleteMany({});
 
-    const camps = [];
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        });
-        camps.push(camp);
-    }
+  const camps = [];
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
 
-    await Campground.insertMany(camps);
-}
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident accusantium voluptatum voluptas tenetur beatae praesentium consequuntur aut, porro sapiente dolore rerum eos atque officia deserunt hic, nostrum unde aliquam repudiandae?',
+      price,
+    });
+    camps.push(camp);
+  }
+
+  await Campground.insertMany(camps);
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
