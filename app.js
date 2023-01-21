a437795c3766196a24489d91d90ddfd0cc61c9d4
Jaimin Parmar <jaimins100@gmail.com>
2023-01-21 11:14:27 +0530

    ejs mate boilerplate template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -16,6 +16,7 @@ db.on('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
