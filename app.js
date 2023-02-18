196cac54d580e34f9a82b2a0300bee1600cb7161
Jaimin Parmar <jaimins100@gmail.com>
2023-02-18 12:07:02 +0530

    creating reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const { campgroundSchema } = require('./schemas');
+const Review = require('./models/review');
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/yelpcamp');
@@ -97,6 +98,18 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  })
+);
+
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema({
+const campgroundSchema = new Schema({
   title: String,
   price: Number,
   image: String,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model('Campground', campgroundSchema);
