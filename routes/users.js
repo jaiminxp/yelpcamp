fb20c814e63209020acbe8dd960c9c20eb1ba5d9
Jaimin Parmar <jaimins100@gmail.com>
2023-02-28 15:37:04 +0530

    login routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
@@ -21,6 +22,22 @@ router.post(
       res.redirect('/register');
     }
   })
+);
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+  }
 );
 
 module.exports = router;
