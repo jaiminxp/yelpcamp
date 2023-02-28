4e18f0da1f39d857c508c483303ff5ff78a9d27b
Jaimin Parmar <jaimins100@gmail.com>
2023-02-28 15:18:16 +0530

    register route login

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/register',
+  catchAsync(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const user = new User({ username, email });
+      const registeredUser = await User.register(user, password);
+      req.flash('success', 'Welcome to YelpCamp!');
+      res.redirect('/campgrounds');
+    } catch (e) {
+      req.flash('error', e.message);
+      res.redirect('/register');
+    }
+  })
+);
 
 module.exports = router;
