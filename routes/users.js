f09d8b0da132bacec30789721d5e37da4fe77c44
Jaimin Parmar <jaimins100@gmail.com>
2023-03-02 07:59:03 +0530

    logout route

--- routes/users.js
+++ routes/users.js
@@ -40,4 +40,14 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+  });
+});
+
 module.exports = router;
