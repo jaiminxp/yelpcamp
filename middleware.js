c41ff1f5e023768e378b5bfc4970bf4c317f6d35
Jaimin Parmar <jaimins100@gmail.com>
2023-03-02 08:51:13 +0530

    redirect to original url after login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must be signed in first!');
+
     return res.redirect('/login');
   }
 
--- routes/users.js
+++ routes/users.js
@@ -38,10 +38,14 @@ router.post(
   passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login',
+    keepSessionInfo: true,
   }),
   (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
 
