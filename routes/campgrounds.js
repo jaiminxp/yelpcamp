8c9554e32b08e95484e1871905846068d08a4778
Jaimin Parmar <jaimins100@gmail.com>
2023-03-13 12:40:15 +0530

    bug fix: save campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -52,6 +52,7 @@ router.post(
     const campground = new Campground(req.body.campground);
     campground.author = req.user._id;
 
+    await campground.save();
     req.flash('success', 'Successfully created a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
   })
