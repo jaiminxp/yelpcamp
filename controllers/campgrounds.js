7ac7a3464877db58a7b5799669b0c3a3c2bdd4a9
Jaimin Parmar <jaimins100@gmail.com>
2023-03-15 19:47:09 +0530

    delete images from campground and cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../lib/cloudinary.config');
 
 async function index(req, res) {
   const campgrounds = await Campground.find({});
@@ -64,6 +65,15 @@ async function updateCampground(req, res) {
   campground.images.push(...imgs);
 
   await campground.save();
+
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
 
   req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${campground._id}`);
