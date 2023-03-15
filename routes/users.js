fb26da82cdc800f864a57231e4373a5762202689
Jaimin Parmar <jaimins100@gmail.com>
2023-03-15 10:17:33 +0530

    restructure routes using router.route

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,22 @@ const passport = require('passport');
 
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router
+  .route('/register')
+  .get(users.renderRegisterForm)
+  .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLoginForm);
-
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login',
-    keepSessionInfo: true,
-  }),
-  users.login
-);
+router
+  .route('/login')
+  .get(users.renderLoginForm)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/login',
+      keepSessionInfo: true,
+    }),
+    users.login
+  );
 
 router.get('/logout', users.logout);
 
