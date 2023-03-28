ab1e4176177667043d4e72c915a0db72ecc91084
Jaimin Parmar <jaimins100@gmail.com>
2023-03-28 14:25:20 +0530

    display a map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,11 @@
 mapboxgl.accessToken = mapToken
+const campground = JSON.parse(campgroundJSON)
 
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 })
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map)
