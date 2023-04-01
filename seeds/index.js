8ea7cb4709bbc1275ec9889fb0659cb8f9cafd74
Jaimin Parmar <jaimins100@gmail.com>
2023-04-01 13:21:20 +0530

    seed 300 campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,7 @@ const seedDB = async () => {
   await User.register(user, 'monkey');
 
   const camps = [];
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20 + 10);
     const campgroundLocation = cities[random1000]
