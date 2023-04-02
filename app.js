7cf7b621749036494ee329920a6a7848f5ada501
Jaimin Parmar <jaimins100@gmail.com>
2023-04-02 16:50:16 +0530

    changed session config

--- app.js
+++ app.js
@@ -40,11 +40,13 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
+  name: "session",
   secret: 'secretkey',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
